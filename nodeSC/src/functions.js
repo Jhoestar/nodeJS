98dc76404a367fe8a47444abf86854b04a9f89b7
Jhoestar <202202883@est.umss.edu>
2024-10-09 17:59:48 -0400

    add

---
+++ nodeSC/src/functions.js
@@ -0,0 +1,10 @@
+
+// export const getTitle = () => 'Curso de Node Js'
+// export const getAutor = (author) => `El autor del curso es ${author}`
+
+
+
+// module.exports = {
+//     getTitle,
+//     getAutor
+// }
---
+++ nodeSC/src/index10.js
@@ -0,0 +1,12 @@
+// import {getTitle, getAutor} from './functions.js'
+// import {curso} from './objects.js'
+
+// const { getTitle,getAutor } = require('./functions')
+// const {curso} = require('./objects')
+// const {nombre,autor} = curso
+
+
+// console.log(getTitle())
+// console.log(getAutor('Jhojan Rios'))
+// console.log(nombre)
+// console.log(autor)
