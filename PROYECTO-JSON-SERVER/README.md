716c2966923d23d6f804c4af6f51977e73f7c9ab
Jhoestar <202202883@est.umss.edu>
2024-10-10 18:19:35 -0400

    node

---
+++ PROYECTO-JSON-SERVER/README.md
@@ -0,0 +1,9 @@
+para realizar este proyecto primero se inicio el proyecto con entry point app.js
+
+el script que se usara es start
+
+se instalara json-server
+    npm i json-server
+se creara db.json con los datos que se desean
+se a;ade en el script 
+    start: "json-server --watch db.json"
--- nodeSC/src/index.js
+++ nodeSC/src/index.js
@@ -1,4 +1,14 @@
-import { config } from "dotenv";
+import { config } from "dotenv"
+// para poder usar las variables de entorno
+import env from 'env-var'
+// para asegurar? la obtencion del puerto
 
 config()
+
+const PORT = env.get('PORT').required().asPortNumber()
+// el puerto se obtiene de la variable PORT 
+// el puerto es requerido
+// el puerto es convertido a numero
+console.log(PORT)
 console.log(process.env.PORT)
+console.log(process.env.JHOJAN_RIOS)
